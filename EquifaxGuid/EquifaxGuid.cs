0ed40cb0435b3b6822479f0a039824024e7a45d1
Aaron Bockover <abock@xamarin.com>
2017-09-10 16:33:24 -0400

    Add more API

--- EquifaxGuid/EquifaxGuid.cs
+++ EquifaxGuid/EquifaxGuid.cs
@@ -1,14 +1,30 @@
-﻿namespace Equifax
+﻿using System;
+
+namespace Equifax
 {
+    /// <summary>
+    /// Creates globally secure Equifax-style GUIDs.
+    /// </summary>
     public static class Guid
     {
-        public static System.Guid NewGuid ()
-        {
-            // use UTC so IDs are globally consistent (greater uniqueness)
-            var t = System.DateTime.UtcNow;
-            return new System.Guid(
+        /// <summary>
+        /// Creates a new Equifax secure GUID using the current UTC time.
+        /// UTC ensures global consistency and uniqueness.
+        /// </summary>
+        public static System.Guid NewGuid()
+            => NewGuid(DateTime.UtcNow);
+
+        /// <summary>
+        /// Creates a new Equifax secure GUID from the provided time stamp,
+        /// assumed to already have been secured for uniqueness.
+        /// </summary>
+        public static System.Guid NewGuid(DateTime secureTimestamp)
+            => new System.Guid(
                 "00000000-0000-0000-0000-00" +
-                $"{t.Month:00}{t.Day:00}{t.Year % 100:00}{t.Hour:00}{t.Minute:00}");
-        }
+                $"{secureTimestamp.Month:00}" +
+                $"{secureTimestamp.Day:00}" +
+                $"{secureTimestamp.Year % 100:00}" +
+                $"{secureTimestamp.Hour:00}" +
+                $"{secureTimestamp.Minute:00}");\
     }
 }
